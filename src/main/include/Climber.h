5d114cd24928279632d36b219d0da693709b775d
Aaron Slamovich <aaronslamovich@gmail.com>
2020-02-03 20:24:46 -0500

    Port Update

--- src/main/include/Climber.h
+++ src/main/include/Climber.h
@@ -2,13 +2,12 @@
 #include "Controls.h"
 #include <ctre/Phoenix.h>
 
-class Climber
-{
-public:
-    Climber(Control *control);
-    void Periodic();
+class Climber {
+    public:
+        Climber(Control *control);
+        void Periodic();
 
-private:
-WPI_TalonSRX *lifter1, *lifter2;
-Control *control;
+    private:
+        WPI_TalonSRX *lifter1, *lifter2;
+        Control *control;
 };
