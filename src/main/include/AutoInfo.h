41dfacd8d527204a663905b70fef677cf8b5785d
Ian Wobschall <ian.wobschall@gmail.com>
2021-03-06 09:32:12 -0500

    Vector things

--- src/main/include/AutoInfo.h
+++ src/main/include/AutoInfo.h
@@ -13,9 +13,30 @@ bool dir;
 float angle;
 float distance;
 };
-std::vector<MoveInfo*> list = {
-new MoveInfo(true,0,200),
-new MoveInfo(false,89,76),
-new MoveInfo(true,89,217),
+std::vector<ra*> list = {
+new MoveInfo(false,0,150),
+new MoveInfo(true,23,130),
+new MoveInfo(false,97,242),
+new MoveInfo(true,71,240),
 };
+std::vector<rb*> list = {
+new MoveInfo(false,23,170),
+new MoveInfo(true,65,190),
+new MoveInfo(false,95,213),
+new MoveInfo(true,45,229),
+}
 };
+
+std::vector<ba*> list = {
+new MoveInfo(true,23.5,380),
+new MoveInfo(false,94,222),
+new MoveInfo(true,110,150),
+new MoveInfo(false,40,50),
+}
+
+std::vector<bb*> list = {
+new MoveInfo(true,20,381),
+new MoveInfo(false,58,150),
+new MoveInfo(true,96,186),
+new MoveInfo(false,40,41),
+}
